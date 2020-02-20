11bddd3f67af804f92eee543da4760a04e1198e6
Peter Flanagan <pflanagan@freewheel.com>
2020-02-20 16:56:29 -0500

    UI for controlling machine

--- src/web/clients/arduino.py
+++ src/web/clients/arduino.py
@@ -1,20 +1,43 @@
 
 import serial
-# import serial.tools.list_ports
+import serial.tools.list_ports
 
 # https://www.youtube.com/watch?v=CebbJ7zrAWU
 # https://stackoverflow.com/questions/24214643/python-to-automatically-select-serial-ports-for-arduino
 
-# ports = list(serial.tools.list_ports.comports())
-# for p in ports:
-#   print(p)
-#   Arduino
 
-SERIAL = serial.Serial('/dev/cu.usbmodem14401', 9600)
+SERIAL = {}
 
+# searches through the ports to find the Arduino
+def findPort():
+  ports = list(serial.tools.list_ports.comports())
+  for p in ports:
+    if 'Arduino' in p and setSerial(p):
+      # if this says arduino and setting it works then stop here
+      return True
+  # if none of those work try this one
+  return setSerial('/dev/cu.usbmodem14401') # TODO: pull from the enviornment
+
+# use this to set a serial port for Arduino, can be called manually
+# port number can be found in the bottom right corner of the Arduino software
+def setSerial(port):
+  try:
+    SERIAL = serial.Serial(port, 9600)
+    return True
+  except:
+    SERIAL = {}
+    return False
+
+# sends a string to the port
 def send(message):
+  # TODO: attach 's-<type>-<timestamp>' before the string
   SERIAL.write(str(message))
   # TODO: wait for a response from the machine (?)
 
+# reads the serial to get info from the machine
 def read():
-  print(SERIAL.read()) # on Arduino: Serial.write(str)
+  print(SERIAL.read()) # TODO: on Arduino: Serial.write(str)
+
+# main
+if __name__ == "__main__":
+  findPort()
---
+++ src/web/static/classes/canvas.js
@@ -0,0 +1,249 @@
+
+// CONST -------------------------------------------------------------------------------------------
+
+const WORLD = {
+  BACKGROUND_COLOR: '#000',
+  Z_AXIS_WIDTH: 120
+};
+
+const TICK = {
+  STROKE_COLOR: '#ddd4',
+  WIDTH: 1,
+  LENGTH: 9
+}
+
+const POINTER = {
+  STROKE_COLOR: '#FFF8',
+  WIDTH: 2,
+  GAP: 12,
+  LENGTH: 24
+};
+
+const BRUSH = {
+  STROKE_COLOR: '#F00',
+  WIDTH: 2
+};
+
+const STROKE = {
+  STROKE_COLOR: '#FF0',
+  WIDTH: 2
+};
+
+const EXAMPLE_COMMANDS = [
+  { type: 'down' },
+  { type: 'to', x: 30, y: 50 },
+  { type: 'to', x: 60, y: 10 },
+  { type: 'to', x: 20, y: 10 },
+  { type: 'up' }
+]
+
+Math.HALF_PI = Math.PI / 2;
+Math.TWO_PI = Math.PI * 2;
+
+// GLOBAL ------------------------------------------------------------------------------------------
+
+function distance(a, b) {
+	return Math.sqrt(
+		Math.pow(a.x - b.x, 2) +
+		Math.pow(a.y - b.y, 2) +
+		Math.pow(a.z - b.z, 2)
+	);
+}
+
+// WORLD -------------------------------------------------------------------------------------------
+
+class World {
+	constructor(ctx, width, height) {
+		this.ctx = ctx;
+		this.W = width;
+    this.H = height;
+    this.stroke = new CanvasStroke(this, ctx);
+    this.pointer = new Pointer(this, ctx);
+    this.zAxis = new ZAxis(this, ctx);
+  }
+  
+  draw({ x, y }) {
+    this.drawBackground();
+    this.stroke.draw({ commands: EXAMPLE_COMMANDS, x, y });
+    this.pointer.draw({ x, y });
+    this.zAxis.draw();
+  }
+
+	drawBackground() {
+		this.ctx.rect(0, 0, this.W, this.H);
+		this.ctx.fillStyle = WORLD.BACKGROUND_COLOR;
+    this.ctx.fill();
+    this.drawTicks();
+  }
+  
+  drawTicks() {
+    for(let y = 10; y < this.H; y += 10){
+      this.ctx.strokeStyle = TICK.STROKE_COLOR;
+      this.ctx.lineWidth = TICK.WIDTH;
+      this.ctx.beginPath();
+      this.ctx.moveTo(0, y);
+      this.ctx.lineTo(TICK.LENGTH, y);
+      this.ctx.stroke();
+      this.ctx.beginPath();
+      this.ctx.moveTo(this.W - WORLD.Z_AXIS_WIDTH, y);
+      this.ctx.lineTo(this.W - WORLD.Z_AXIS_WIDTH - TICK.LENGTH, y);
+      this.ctx.stroke();
+    }
+    for(let x = 10; x < this.W - WORLD.Z_AXIS_WIDTH; x += 10){
+      this.ctx.strokeStyle = TICK.STROKE_COLOR;
+      this.ctx.lineWidth = TICK.WIDTH;
+      this.ctx.beginPath();
+      this.ctx.moveTo(x, 0);
+      this.ctx.lineTo(x, TICK.LENGTH);
+      this.ctx.stroke();
+      this.ctx.beginPath();
+      this.ctx.moveTo(x, this.H);
+      this.ctx.lineTo(x, this.H - TICK.LENGTH);
+      this.ctx.stroke();
+    }
+  }
+
+}
+
+// STROKE ------------------------------------------------------------------------------------------
+
+class CanvasStroke {
+	constructor(world, ctx) {
+		this.world = world;
+    this.ctx = ctx;
+	}
+
+	draw({ commands, x, y }) {
+    this.ctx.strokeStyle = STROKE.STROKE_COLOR;
+    this.ctx.lineWidth = STROKE.WIDTH;
+    commands.forEach(command => {
+      switch (command.type) {
+      case 'down':
+        this.ctx.beginPath();
+        this.ctx.moveTo(x, y);
+        break;
+      case 'to':
+        this.ctx.lineTo(command.x, command.y);
+        break;
+      case 'up':
+        this.ctx.stroke();
+        this.ctx.closePath();
+        break;
+      }
+    });
+	}
+}
+
+// POINTER ------------------------------------------------------------------------------------------
+
+class Pointer {
+	constructor(world, ctx) {
+		this.world = world;
+    this.ctx = ctx;
+	}
+
+	draw({ x, y }) {
+    this.drawLines({ x, y });
+    this.drawBrush({ x, y , a, b });
+	}
+
+  drawLines({ x, y }) {
+		this.ctx.strokeStyle = POINTER.STROKE_COLOR;
+		this.ctx.lineWidth = POINTER.WIDTH;
+		this.ctx.beginPath();
+		this.ctx.moveTo(x, y + POINTER.GAP);
+		this.ctx.lineTo(x, y + POINTER.GAP + POINTER.LENGTH);
+		this.ctx.stroke();
+		this.ctx.beginPath();
+		this.ctx.moveTo(x, y - POINTER.GAP);
+		this.ctx.lineTo(x, y - POINTER.GAP - POINTER.LENGTH);
+		this.ctx.stroke();
+		this.ctx.beginPath();
+		this.ctx.moveTo(x + POINTER.GAP, y);
+		this.ctx.lineTo(x + POINTER.GAP + POINTER.LENGTH, y);
+		this.ctx.stroke();
+		this.ctx.beginPath();
+		this.ctx.moveTo(x - POINTER.GAP, y);
+		this.ctx.lineTo(x - POINTER.GAP - POINTER.LENGTH, y);
+    this.ctx.stroke();
+  }
+
+  drawBrush({ x, y, a, b }) {
+		this.ctx.strokeStyle = BRUSH.STROKE_COLOR;
+		this.ctx.lineWidth = BRUSH.WIDTH;
+		this.ctx.beginPath();
+    this.ctx.moveTo(x, y);
+    // TODO: calculate based on angles
+		this.ctx.lineTo(x + 16, y + 24);
+    this.ctx.stroke();
+
+    // this.ctx.beginPath();
+    // this.ctx.arc(x + 16, y + 24, 2, 0, Math.TWO_PI);
+    // this.ctx.fillStyle = BRUSH.STROKE_COLOR;
+    // this.ctx.fill();
+  }
+}
+
+// Z-AXIS ------------------------------------------------------------------------------------------
+
+class ZAxis {
+  constructor(world, ctx) {
+    this.world = world;
+    this.ctx = ctx;
+  }
+
+  draw() {
+    this.drawBackground();
+    this.drawBrush({ z: .8 });
+  }
+
+  drawBackground() {
+		this.ctx.rect(this.world.W - WORLD.Z_AXIS_WIDTH, 0, this.world.W, this.world.H);
+		this.ctx.fillStyle = WORLD.BACKGROUND_COLOR;
+    this.ctx.fill();
+    for(let y = 10; y < this.world.H; y += 10){
+      this.ctx.strokeStyle = TICK.STROKE_COLOR;
+      this.ctx.lineWidth = TICK.WIDTH;
+      this.ctx.beginPath();
+      this.ctx.moveTo(this.world.W, y);
+
+      let end = this.world.W - TICK.LENGTH;
+      // if (y % 50 === 0) end = this.world.W - 100;
+      if (y > this.world.H - 60) end = this.world.W - WORLD.Z_AXIS_WIDTH;
+
+      this.ctx.lineTo(end, y);
+      this.ctx.stroke();
+    }
+  }
+
+  drawBrush({ z }) {
+    const x = this.world.W - (WORLD.Z_AXIS_WIDTH / 2);
+		this.ctx.strokeStyle = BRUSH.STROKE_COLOR;
+		this.ctx.lineWidth = BRUSH.WIDTH;
+		this.ctx.beginPath();
+    this.ctx.moveTo(x, 0);
+    // TODO: calculate based on angles
+		this.ctx.lineTo(x + 20, this.world.H * z);
+    this.ctx.stroke();
+    
+    this.ctx.beginPath();
+    this.ctx.arc(x + 20, this.world.H * z, 2, 0, Math.TWO_PI);
+    this.ctx.fillStyle = BRUSH.STROKE_COLOR;
+    this.ctx.fill();
+  }
+}
+
+// MAIN --------------------------------------------------------------------------------------------
+
+window.onload = function () {
+	const canvas = document.getElementById("canvas");
+	const ctx = canvas.getContext("2d");
+
+  const W = 570, H = 300;
+  canvas.width = W;
+  canvas.height = H;
+
+  const world = new World(ctx, W, H);
+  world.draw({ x: 120, y: 80 }); // z, a, b, stroke
+}
+
